9d0db6d51b5333cd4fa1532af31aea531eca2259
Romain Thomas <romain.thomas@sheffield.ac.uk>
2026-01-21 21:54:53 +0000

    create doc

---
+++ docs/source/conf.py
@@ -0,0 +1,30 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'grunt'
+copyright = '2026, R. Thomas'
+author = 'R. Thomas'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_rtd_theme"
+html_logo = "logo/logo.png"
+html_favicon = 'logo/favicon.ico'  
+html_static_path = ['_static']
---
+++ docs/source/installation.rst
@@ -0,0 +1,27 @@
+Installation
+============
+
+Dependencies
+------------
+GRUNT is written entirely in Python. To make it work you will neec the following packages:
+
+.. danger::
+
+    I advise you to create a dedicated python environment when you install GRUNT. See `here <https://docs.python.org/3/library/venv.html>`_ for more details.
+
+
+Install from pip
+----------------
+
+
+Install from github
+-------------------
+
+To install from github you can:
+
+* Clone the `repository <https://github.com/Romain-Thomas-Shef/grunt>`_ and create a local install from it
+* Use pip to install it directly from github:
+
+.. code-block:: shell
+
+    pip install git+https://github.com/Romain-Thomas-Shef/grunt
